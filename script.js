8c1873fcffe5a4251b9be6a537f2c8a46cd2d7ba
sakshi jarag <sakshi.jarag@mitwpu.edu.in>
2025-07-28 20:20:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JS functionality will go here (e.g. scroll animations, form handling)
+
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+});
